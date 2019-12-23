85e455a2982bafa04d4ae9a53b71b5a0f038bda3
Artem <LD31D@github.com>
2019-12-23 23:23:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,48 @@
-from flask import Flask, render_template, redirect
+from db import DataBase
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
+db = DataBase()
 
 
 @app.route('/')
 def main():
+	music = db.return_table()
 	return render_template(
-		'index.html'
+		'index.html', music=music
 	)
+
+
+@app.route('/delete-music/', methods=['POST'])
+def delete_music():
+	db.delete(request.form["id"])
+	return redirect('/')
+
+
+@app.route('/add-music/', methods=['POST'])
+def add_music():
+	autor = request.form["autor"]
+	name = request.form["name"]
+	minutes = request.form["min"]
+	seconds = request.form["sec"]
+
+	if (len(autor.replace(" ", "")) != 0 
+	and len(name.replace(" ", "")) != 0 
+	and minutes.isdigit() 
+	and seconds.isdigit() 
+	and 0 <= int(minutes) <= 180 
+	and 0 <= int(seconds) <= 59):
+		if len("0" + seconds) == 2:
+			seconds = "0" + seconds
+		db.write(autor, name, minutes + ":" + seconds)
+
+	else:
+		music = db.return_table()
+		return render_template(
+		'index.html', music=music, error=True
+		)
+
+	return redirect('/')
 
 
 @app.errorhandler(404)
