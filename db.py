0cad4236438504b6a3138ae321eb39f093139bf7
Artem <LD31D@github.com>
2019-12-23 22:28:29 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,10 +1,10 @@
 import sqlite3
 
 class DataBase:
-	conn = sqlite3.connect('db.sqlite3')
-	cursor = conn.cursor()
 
 	def __init__(self):
+		self.create_connect()
+
 		try:
 			self.cursor.execute(''' CREATE TABLE music (
 													id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,17 +17,39 @@ class DataBase:
 		except sqlite3.OperationalError:
 			pass
 
+		self.break_connection()
+
+	def create_connect(self):
+		self.conn = sqlite3.connect('db.sqlite3', check_same_thread = False)
+		self.cursor = self.conn.cursor()
+
+	def break_connection(self):
+		self.cursor.close()
+		self.conn.close()
+
 	def write(self, autor, name, len):
+		self.create_connect()
+
 		self.cursor.execute(f''' INSERT INTO music (autor, name, len) 
 									VALUES ("{autor}", "{name}", "{len}") ''')
 		self.conn.commit()
 
+		self.break_connection()
+
 	def delete(self, id):
+		self.create_connect()
+
 		self.cursor.execute(f' DELETE FROM music WHERE id={id} ')
 		self.conn.commit()
 
+		self.break_connection()
+
 	def return_table(self):
+		self.create_connect()
+
 		self.cursor.execute(' SELECT * FROM music ')
-		table = self.cursor.fetchone()
+		table = self.cursor.fetchall()
+
+		self.break_connection()
 
 		return table
