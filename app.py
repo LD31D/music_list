4e6053e5b4f07f8669c0f339bd824600a6c65afa
Artem <LD31D@github.com>
2019-12-23 11:25:26 +0200

    Deleting templates/404.html

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main():
-	return "Hello"
+	return render_template(
+		'index.html'
+	)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return redirect('/')
 
 
 if __name__ == '__main__':
